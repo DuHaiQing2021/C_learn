b9f608cdb74a598816ab756599dd77e1b56f4fee
DuHaiQing2021 <792482948@qq.com>
2021-09-10 21:45:59 +0800

    2021.9.10

---
+++ test21.9.10/test21.9.10/test9.10.c
@@ -0,0 +1,79 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+////代码1
+//int main()
+//{
+//    int age = 0;
+//    scanf("%d", &age);
+//    if (age < 18)
+//    {
+//        printf("未成年\n");
+//    }
+////}
+////代码2
+//#include <stdio.h>
+//int main()
+//{
+//    int age = 0;
+//    scanf("%d", &age);
+//    if (age < 18)
+//    {
+//        printf("未成年\n");
+//    }
+//    else
+//    {
+//        printf("成年\n");
+//    }
+//}
+//代码3
+//#include <stdio.h>
+//int main()
+//{
+//    int age = 0;
+//    scanf("%d", &age);
+//    if (age < 18)
+//    {
+//        printf("少年\n");
+//    }
+//    else if (age >= 18 && age < 30)
+//    {
+//        printf("青年\n");
+//    }
+//    else if (age >= 30 && age < 50)
+//    {
+//        printf("中年\n");
+//    }
+//    else if (age >= 50 && age < 80)
+//    {
+//        printf("老年\n");
+//    }
+//    else
+//    {
+//        printf("老寿星\n");
+//    }
+//
+//    return 0;
+//}
+
+
+/*switch语句*/
+//#include<stdio.h>
+//int main() {
+//
+//	int day;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1: 
+//		printf("星期一\n"); 
+//	    break;
+//	case 2:
+//		printf("星期二\n");
+//		break;
+//	default:printf("找不到\n");
+//	}
+//	return 0;
+//}
+
+
