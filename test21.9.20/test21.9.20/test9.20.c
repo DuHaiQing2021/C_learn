07993088c3b3a5a4eaa056adb218ac6af3590652
DuHaiQing2021 <792482948@qq.com>
2021-09-20 23:08:34 +0800

    2021.9.20

---
+++ test21.9.20/test21.9.20/test9.20.c
@@ -0,0 +1,91 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<Stdio.h>
+#include<stdlib.h>
+#include<time.h>
+//void meau()
+//{
+//	printf("********************猜数字游戏*********************\n");
+//	printf("***************************************************\n");
+//	printf("********************1.进入游戏*********************\n");
+//	printf("********************0.退出游戏*********************\n");
+//	printf("***************************************************\n");
+//}
+//
+//void game()
+//{
+//	int guess = 0;
+//	int ret = rand() % 100 + 1;
+//	int temp = 0;
+//	printf("请猜数字：>>>");
+//	do
+//	{
+//		scanf("%d", &guess);
+//		if (guess < ret)
+//		{
+//			printf("猜小了！\n\n");
+//		}
+//		else if (guess > ret)
+//		{
+//			printf("猜大了！\n\n");
+//		}
+//		else if (guess == ret)
+//		{
+//			printf("恭喜猜中了！\n\n");
+//			break;
+//		}
+//
+//	} while (guess != ret);
+//	printf("是否继续游戏？（1继续 0退出）>>>");
+//
+//}
+//
+//
+//
+//int main()
+//{
+//
+//	int input = 0;
+//
+//	srand((unsigned int)time(NULL));//设计随机数的生成起点
+//
+//	do
+//	{
+//		meau();
+//		printf("请选择:（1进入游戏 0退出游戏）>>>");
+//		scanf("%d",&input);
+//		switch (input)
+//		{
+//		case 0:
+//			printf("退出游戏！\n\n");
+//			break;
+//		case 1:
+//			printf("进入游戏！\n\n");
+//			break;
+//		default:
+//			printf("输入错误重新输入！\n\n");
+//		}
+//		if (input == 1)
+//		{
+//			int temp = 1;
+//			while (temp)
+//			{
+//				game();
+//				scanf("%d", &temp);
+//			}
+//			input = temp;
+//		}
+//
+//	} while (input);
+//
+//	return 0;
+//}
+
+
+#include<stdio.h>
+
+int main()
+{
+
+
+	return 0;
+}
