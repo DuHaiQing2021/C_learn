7d0fd9d172d583712a9eb5e8d0d2c4eb4aac3f80
DuHaiQing2021 <792482948@qq.com>
2021-10-08 19:51:52 +0800

    2021.10.8

---
+++ test21.10.8/test21.10.8/test10.8.c
@@ -0,0 +1,70 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include<Stdio.h>
+//
+//
+//
+//#define MAX 1000
+//#define reg register          //为 register这个关键字，创建一个简短的名字
+//#define do_forever for(;;)     //用更形象的符号来替换一种实现
+//#define CASE break;case        //在写case语句的时候自动把 break写上。
+//// 如果定义的 stuff过长，可以分成几行写，除了最后一行外，每行的后面都加一个反斜杠(续行符)。
+//#define DEBUG_PRINT printf("file:%s\tline:%d\t \
+//                          date:%s\ttime:%s\n" ,\
+//__FILE__,__LINE__ ,       \
+//__DATE__,__TIME__ )  
+//
+//
+//
+//int main()
+//{
+//	printf("%s\n", __FILE__);
+//	printf("%ld\n", __LINE__);
+//	printf("%s\n", __DATE__);
+//	printf("%s\n", __TIME__);
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+//SQT1\SQT2是宏名。
+//#define SQT1(x) x*x
+//#define SQT2(x) ((x)*(x))
+//
+//int main()
+//{
+//	int a = 5;
+//	int ret1 = SQT1(a+5);    //a+5*a+5
+//	int ret2 = SQT2(a + 5);  //((a+5)*(a+5))
+//
+//
+//	printf("%d\n", ret1);
+//	printf("%d\n", ret2);
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
